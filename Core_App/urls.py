3c8c89dadc053b274316332d57165c72517afd0b
Md Zahid Hassan <46865100+zh-cse18@users.noreply.github.com>
2021-06-08 10:05:43 +0600

    change url path name

--- Core_App/urls.py
+++ Core_App/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('contact/', include('contacts.urls')),
+    path('account', include('accounts.urls')),
+    path('', include('contacts.urls')),
     path('token/', TokenObtainPairView.as_view()),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- contacts/urls.py
+++ contacts/urls.py
@@ -6,5 +6,5 @@ from contacts.views import ContactAPIView, SearchView, home
 urlpatterns = [
     path('newContact/', ContactAPIView.as_view(), name='contact'),
     path('search/', SearchView.as_view(), name='search'),
-    path('home/', home, name='home'),
+    path('', home, name='home'),
 ]
